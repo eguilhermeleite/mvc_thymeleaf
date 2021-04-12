719a3777364aac7156617c41984e1388196ca2ed
Edvaldo Leite <eguilhermeleite@gmail.com>
2021-04-12 20:12:39 -0300

    Projeto Criado

---
+++ projeto_mvc_thymeleaf/src/main/java/com/edvaldo/leite/ProjetoMvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.edvaldo.leite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ projeto_mvc_thymeleaf/src/test/java/com/edvaldo/leite/ProjetoMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edvaldo.leite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
